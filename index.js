e2be18ca18b7e340526cc2e69208f370208c51c0
David Caballero <davidcame124@gmail.com>
2023-11-16 12:35:50 -0500

    version 1.1

---
+++ index.js
@@ -0,0 +1,11 @@
+function scrollToSection(sectionId) {
+    var section = document.getElementById(sectionId);
+
+    if (section) {
+      // Usa el método scrollIntoView para desplazarte al elemento
+      section.scrollIntoView({
+        behavior: 'smooth', // Esto proporciona un desplazamiento suave
+        block: 'start'       // Esto alinea el inicio del elemento con la parte superior del contenedor de desplazamiento
+      });
+    }
+  }
